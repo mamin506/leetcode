efeea034f15d1388fcde97947daf3dbd94cbd323
Min Ma <mamin506@gmail.com>
2015-07-04 00:24:10 +0800

    new solved cases

--- RemoveElement/Solution.cpp
+++ RemoveElement/Solution.cpp
@@ -28,6 +28,18 @@ public:
         }
         return result;
     }
+
+    // solution2
+    //int removeElement(vector<int>& nums, int val) {
+    //    int cnt = 0;
+    //    for(int i = 0 ; i < nums.size() ; ++i) {
+    //        if(nums[i] == val)
+    //            cnt++;
+    //        else
+    //            nums[i-cnt] = nums[i];
+    //    }
+    //    return nums.size()-cnt;
+    //}
 };
 
 int main(int argc, char *argv[]) {
