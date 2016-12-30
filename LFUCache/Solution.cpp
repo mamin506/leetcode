fb3c17fc4a0bc15596339ab189cea15e4c299213
mamin506 <mamin506@gmail.com>
2016-12-30 15:31:52 +0800

    new cases, LFU is not finished

---
+++ LFUCache/Solution.cpp
@@ -0,0 +1,26 @@
+#inlcude "../mlibs.h"
+
+using namespace std;
+
+class LFUCache {
+private:
+    int cap;
+
+public:
+    LFUCache(int capacity) {
+        cap = capacity;
+    }
+
+    int get(int key) {
+    }
+
+    void set(int key, int value) {
+    }
+
+};
+
+int main(int argc, char *argv[]) {
+    Solution sol1;
+    return 0;
+}
+
---
+++ ReverseNodesInK-Group/Solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../mlibs.h"
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+
+    }
+};
+
+int main(int argc, char *argv[]) {
+    class Solution sol1;
+
+    return 0;
+}
+
